c2590f91cb4ed37b3033f7626f4256ac03ba4d28
ABBA3124 <davideabbadessa73@gmail.com>
2024-05-09 16:15:35 +0200

    upgrade

--- src/components/ArticleCard.tsx
+++ src/components/ArticleCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Result } from '../types'
+import { Col } from 'react-bootstrap'
 
 interface ArticleCardProps {
   Result: Result
@@ -7,14 +8,18 @@ interface ArticleCardProps {
 
 const ArticleCard: React.FC<ArticleCardProps> = ({ Result }) => {
   return (
-    <div className="card">
-        <h5 className="card-title">{Result.title}</h5>
-      <img src={Result.image_url} alt={Result.title} className="card-img-top w-25 rounded-5" />
+    <Col xs={12} sm={6} md={6} lg={4} xl={3} xxl={3}>
+    <div className="card border-2 border-light h-100">
+      <img src={Result.image_url} alt={Result.title} style={{height: "300px"}} className="rounded-5 m-3 img-fluid" />
       <div className="card-body">
+        <h5 className="card-title mb-3">{Result.title}</h5>
+        <hr />
         <p className="card-text">{Result.summary}</p>
+        <hr />
         <p className="card-text"><small className="text-muted">Published on {new Date(Result.published_at).toLocaleDateString()}</small></p>
       </div>
     </div>
+    </Col>
   )
 }
 
--- src/routes/HomePage.tsx
+++ src/routes/HomePage.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { Result } from '../types/index'
 import ArticleCard from '../components/ArticleCard'
+import { Row } from 'react-bootstrap'
 
 const HomePage: React.FC = () => {
   const [articles, setArticles] = useState<Result[]>([])
@@ -13,11 +14,15 @@ const HomePage: React.FC = () => {
   }, [])
 
   return (
-    <div>
-        <h1>prova homepage si vede?</h1>
+    <div className='bg-secondary'>
+    <div className='m-5'>
+        <h2 className='text-center mb-5 mt-3'>⬇️Tutti i nostri articoli !⬇️</h2>
+        <Row className='row-gap-3'>
       {articles.map(result => (
         <ArticleCard key={result.id} Result={result} />
       ))}
+      </Row>
+    </div>
     </div>
   )
 }
